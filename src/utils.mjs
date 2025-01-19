d72faf36b7a8e404db9207a74cb7be8b66ce7e66
Chasyxx <creset200@gmail.com>
2025-01-18 20:24:31 -0700

    Differentiate between Kilobytes and Kibibytes

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,7 +1,11 @@
 export function formatBytes(bytes) {
-	if(bytes < 1E4) {
+	if(bytes < 2e3) {
 		return bytes + 'B';
 	}
-	const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-	return (i ? (bytes / (1024 ** i)).toFixed(2) : bytes) + ['B', 'KB', 'MB', 'GB', 'TB'][i];
+	// i fear the day we get a 1 Terabyte code. - Chasyxx, creator of the EnBeat_NEW fork
+	const power1000i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)), 10);
+	const power1000s = (power1000i ? (bytes / (1000 ** power1000i)).toFixed(2) : bytes) + ['B', 'KB', 'MB', 'GB', 'TB'][power1000i];
+	const power1024i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+	const power1024s = (power1000i ? (bytes / (1024 ** power1000i)).toFixed(2) : bytes) + ['B', 'KiB', 'MiB', 'GiB', 'TiB'][power1024i];
+	return `${power1024s} (${[power1000s]})`
 }
